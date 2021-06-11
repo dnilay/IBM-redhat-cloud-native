370f6261b8fba644b7ce872f483538e42ac2a86b
nilay <nilay@nilay-Latitude-E6320>
2021-06-11 14:27:53 +0530

    first commit

--- exception_demo/src/com/example/app/App.java
+++ exception_demo/src/com/example/app/App.java
@@ -18,6 +18,15 @@ public class App {
 
 	public static void main(String args[]) {
 		App app = new App();
+		try {
+			
+		} 
+		catch(Exception e) {
+			// TODO: handle finally clause
+		}
+		finally {
+			
+		}
 		/*
 		 * for(int i=0;i<2;i++) { try { System.out.print("ToDo Desc: "); String
 		 * tempDesc=scanner.next(); System.out.print("isCompleted? "); boolean
@@ -38,16 +47,24 @@ public class App {
 		 * 
 		 * }
 		 */
-		Employee employee = null;
-		Manager manager = new Manager();
-		app.check(manager);
+		/*
+		 * Employee employee = null; Manager manager = new Manager(); if(employee==null)
+		 * { throw new NullPointerException("Null Pointer Exception Occured"); }
+		 * app.check(manager);
+		 */
+		/*
+		 * try { app.check(employee);
+		 * 
+		 * } catch (NullPointerException e) { e.printStackTrace(); }
+		 */
+		
+		// app.check(employee);
 
-		app.check(employee);
-
+		app.div(100, 0);
 	}
 
-	private void check(Employee o) {
-		try {
+	private void check(Employee o)throws NullPointerException {
+	
 		//	int a = 100 / 0;
 			if (o instanceof Manager) {
 				Manager manager = (Manager) o;
@@ -57,21 +74,15 @@ public class App {
 			}
 
 		}
+	
+	private void div(int a,int b)throws ArithmeticException
+	{
+		System.out.println(a/b);
+	}
+	
+	
 
-		/*
-		 * catch (NullPointerException e) {
-		 * System.err.println("null pointer exception"); e.printStackTrace(); }
-		 */catch (ArrayIndexOutOfBoundsException e) {
-			System.err.println("array index out of bound exception");
-		}
-		catch (ArithmeticException e) {
-			System.err.println("arithemetic");
-			e.printStackTrace();
-		}
-
-		/*
-		 * catch (Exception e) { System.err.println(e.toString()); }
-		 */
+		
 	}
 
-}
+
