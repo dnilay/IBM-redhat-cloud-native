66da9f1e83b077e85cb28c22e7a4c33feebff35a
Nilay <54245048+dnilay@users.noreply.github.com>
2021-06-15 09:22:31 +0530

    Add files via upload

---
+++ Q3/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] str) 
+	  {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Please enter the password you want to set :");
+	    String password=sc.nextLine();
+	    if(UesrMainCode.validatePassword(password))
+	      System.out.println("The password is valid..");
+	    else
+	      System.out.println("The password is invalid..");
+	   }
+
+}
