80bab798046ae300e22958d9648e52aa8a3f0507
nilay <nilay@nilay-Latitude-E6320>
2021-06-17 12:33:29 +0530

    added sync

---
+++ 17_06_21/src/comm/example/race/ThreadTest.java
@@ -0,0 +1,12 @@
+package comm.example.race;
+
+public class ThreadTest {
+
+	public static void main(String[] args) {
+		new PrintStringThread("Hello", "There");
+		new PrintStringThread("How Are", "You?");
+		new PrintStringThread("Thank You", "Very Much");
+
+	}
+
+}
