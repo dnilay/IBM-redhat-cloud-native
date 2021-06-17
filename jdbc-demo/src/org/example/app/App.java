2b426913fa04c567d15e292ac67475fa1f225960
nilay <nilay@nilay-Latitude-E6320>
2021-06-17 17:51:50 +0530

    added service layer

--- jdbc-demo/src/org/example/app/App.java
+++ jdbc-demo/src/org/example/app/App.java
@@ -7,12 +7,14 @@ import java.util.List;
 import org.example.dao.EmployeeDao;
 import org.example.dao.EmployeeDaoImpl;
 import org.example.model.Employee;
+import org.example.service.EmployeeService;
+import org.example.service.EmployeeServiceImpl;
 
 public class App {
 
 	public static void main(String[] args) throws SQLException {
-		EmployeeDao dao=new EmployeeDaoImpl();
-		List<Employee> employees=dao.getAllEmployees();
+	EmployeeService service=new EmployeeServiceImpl();
+		List<Employee> employees=service.getAllEmployees();
 		Iterator<Employee> iterator=employees.iterator();
 		while(iterator.hasNext())
 			System.out.println(iterator.next());
