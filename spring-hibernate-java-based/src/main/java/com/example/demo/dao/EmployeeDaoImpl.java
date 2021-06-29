967395e2ad9b80ebf20db12a5d6f3e3ba0c6b322
Nilay <54245048+dnilay@users.noreply.github.com>
2021-06-29 17:37:47 +0530

    Add files via upload

--- spring-hibernate-java-based/src/main/java/com/example/demo/dao/EmployeeDaoImpl.java
+++ spring-hibernate-java-based/src/main/java/com/example/demo/dao/EmployeeDaoImpl.java
@@ -2,6 +2,12 @@ package com.example.demo.dao;
 
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.Query;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.modelmapper.ModelMapper;
@@ -16,7 +22,7 @@ import com.example.demo.entity.Employee;
 import com.example.demo.ui.EmployeeResponseModel;
 
 @Repository
-@EnableTransactionManagement
+
 public class EmployeeDaoImpl implements EmployeeDao {
 
 	private SessionFactory sessionFactory;
@@ -36,12 +42,53 @@ public class EmployeeDaoImpl implements EmployeeDao {
 		// TODO Auto-generated method stub
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		Employee employee = modelMapper.map(employeeDetails, Employee.class);
-		Session currentSession = sessionFactory.getCurrentSession();
-		currentSession.getTransaction().begin();
+		Session currentSession = sessionFactory.openSession();
+	currentSession.getTransaction().begin();
 		currentSession.persist(employee);
 		currentSession.getTransaction().commit();
 		EmployeeResponseModel model = modelMapper.map(employee, EmployeeResponseModel.class);
 		return model;
+	}
+
+	@Override
+	public List<EmployeeResponseModel> displayAllEmployee() {
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		Session currentSession = sessionFactory.openSession();
+		Query query=currentSession.createQuery("SELECT E FROM Employee E");
+		@SuppressWarnings("unchecked")
+		List<Employee> list=query.getResultList();
+		Iterator<Employee> i=list.iterator();
+		List<EmployeeResponseModel> list1=new ArrayList<EmployeeResponseModel>();
+		while(i.hasNext())
+		{
+			list1.add(modelMapper.map(i.next(), EmployeeResponseModel.class));		}
+		return list1;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public EmployeeResponseModel findByEmployeeId(String employeeId) {
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		Session currentSession = sessionFactory.openSession();
+		Query query=currentSession.createQuery("SELECT E FROM Employee E WHERE E.employeeId=:empId").setString("empId", employeeId);
+		List<Employee> list=query.getResultList();
+		if(list.isEmpty())
+		{
+			
+			return null;
+		}
+		else
+		{
+			Iterator<Employee> i=list.iterator();
+			List<EmployeeResponseModel> list1=new ArrayList<EmployeeResponseModel>();
+			while(i.hasNext())
+			{
+				list1.add(modelMapper.map(i.next(), EmployeeResponseModel.class));		
+			}
+			EmployeeResponseModel model=list1.get(0);
+			return model;
+		}
+		
 	}
 
 }
