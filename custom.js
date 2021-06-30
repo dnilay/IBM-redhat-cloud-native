cd563eb5fed8b415f38aa459b7fa977f2d91ce10
Nilay Das <mailnilaydas@gmail.com>
2021-06-30 10:36:44 +0530

    first commit

---
+++ custom.js
@@ -0,0 +1,27 @@
+//console.log('hello world');
+
+//console.log("hello from js")
+
+// Addition operator
+console.log(4 + 8);
+
+// Subtraction operator
+console.log(4 - 8);
+
+// Multiplication operator
+console.log(4 * 8);
+
+// Division operator
+console.log(4 / 8);
+
+// Modulus operator
+console.log(7 % 3);
+
+console.log('hello world');
+
+
+
+
+
+
+
