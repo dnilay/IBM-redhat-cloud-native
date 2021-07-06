8cbe48da3d8de564cdf4a66842f7e312083f44fa
Nilay <54245048+dnilay@users.noreply.github.com>
2021-07-06 14:07:27 +0530

    Add files via upload

--- before/vidly/routes/customers.js
+++ before/vidly/routes/customers.js
@@ -21,27 +21,7 @@ router.post('/', async (req, res) => {
   
   res.send(customer);
 });
-router.get('/:id', async (req, res) => {
-  const customer = await Customer.findById(req.params.id)
-  .catch((err)=>{
-    res.status(404).send('The customer with the given ID was not found.');
-  });
-  if (!customer) 
-  return res.status(404).send('The customer with the given ID was not found.');
 
-  res.send(customer);
-
-});
-router.delete('/:id', async (req, res) => {
-  const customer = await Customer.findByIdAndRemove(req.params.id).catch((err)=>{
-    res.status(404).send('The customer with the given ID was not found.');
-  });
-
-  if (!customer) 
-    return res.status(404).send('The customer with the given ID was not found.');
-
-  res.send(customer);
-});
 router.put('/:id', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
@@ -58,7 +38,20 @@ router.put('/:id', async (req, res) => {
   res.send(customer);
 });
 
+router.delete('/:id', async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params.id);
 
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
 
+  res.send(customer);
+});
+
+router.get('/:id', async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+
+  if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+  res.send(customer);
+});
 
 module.exports = router; 
--- before/vidly/routes/genres.js
+++ before/vidly/routes/genres.js
@@ -17,26 +17,34 @@ router.post('/', async (req, res) => {
   
   res.send(genre);
 });
-router.get('/:id', async (req, res) => {
-  const genre = await Genre.findById(req.params.id).catch((err)=>{
-    res.status(404).send('The genre with the given ID was not found.');
 
+router.put('/:id', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+    new: true
   });
 
-  if (!genre) 
-    return res.status(404).send('The genre with the given ID was not found.');
-
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+  
   res.send(genre);
 });
 
 router.delete('/:id', async (req, res) => {
-  const genre = await Customer.findByIdAndRemove(req.params.id).catch((err)=>{
-    res.status(404).send('The genre with the given ID was not found.');
-  });
+  const genre = await Genre.findByIdAndRemove(req.params.id);
 
-  if (!genre) 
-    return res.status(404).send('The genre with the given ID was not found.');
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
 });
+
+router.get('/:id', async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+  res.send(genre);
+});
+
 module.exports = router;
