d13b6c5af5d1076b006d17f197aa70011d057e46
Nilay <54245048+dnilay@users.noreply.github.com>
2021-07-12 13:12:57 +0530

    Add files via upload

--- discovery-service-project/src/main/java/com/example/demo/config/MySecurityConfig.java
+++ discovery-service-project/src/main/java/com/example/demo/config/MySecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @EnableWebSecurity
 @Configuration
