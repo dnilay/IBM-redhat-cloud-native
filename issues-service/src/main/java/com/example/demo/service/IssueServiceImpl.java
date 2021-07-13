c128e905deaaea5141118fdd4df9fdd8b0d5a59d
Nilay <54245048+dnilay@users.noreply.github.com>
2021-07-13 13:07:03 +0530

    Add files via upload

--- issues-service/src/main/java/com/example/demo/service/IssueServiceImpl.java
+++ issues-service/src/main/java/com/example/demo/service/IssueServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.exception.IssueNotFoundException;
 import com.example.demo.model.Issue;
 import com.example.demo.repo.IssueRepository;
 
@@ -32,6 +34,45 @@ public class IssueServiceImpl implements IssueService {
 	public List<Issue> displayAllIssues() {
 		log.info("within display issue service");
 		return issueRepository.findAll();
+	}
+
+	@Override
+	public Optional<Issue> findIssueById(String id) {
+		Optional<Issue> issue=issueRepository.findById(id);
+		if(issue.isEmpty())
+		{
+			throw new IssueNotFoundException("isue with the given id not found.");
+		}
+		
+		return issue;
+	}
+
+	@Override
+	public Issue updateIssue(String id, Issue issue) {
+		Optional<Issue> issue1=issueRepository.findById(id);
+		if(issue1.isEmpty())
+		{
+			throw new IssueNotFoundException("isue with the given id not found.");
+		}
+		Issue tempIssue=issue1.get();
+		tempIssue.setSeverity(issue.getSeverity());
+		tempIssue.setAssignee(issue.getAssignee());
+		tempIssue.setDescription(issue.getAssignee());
+		issueRepository.save(tempIssue);
+		log.info("updated");
+		return tempIssue;
+	}
+
+	@Override
+	public Issue removeIssue(String id) {
+		Optional<Issue> issue1=issueRepository.findById(id);
+		if(issue1.isEmpty())
+		{
+			throw new IssueNotFoundException("isue with the given id not found.");
+		}
+		Issue tempIssue=issue1.get();
+		issueRepository.delete(tempIssue);
+		return tempIssue;
 	}
 
 	
